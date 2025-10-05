44d0e2bf3520bda6ef566059cd0ae4dc379cde9f
Azfdfkhzz <AzFdFkhz@gmail.com>
2025-10-05 18:54:47 +0700

    Delete src/api directory

--- src/api/ghibli.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_BASE = "https://ghibliapi.vercel.app";
-
-export const getFilms = async () => {
-  const res = await axios.get(`${API_BASE}/films`);
-  return res.data;
-};
